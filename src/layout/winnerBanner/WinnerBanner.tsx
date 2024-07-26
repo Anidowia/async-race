29772de21827a9d3aec72b7a002d204308d47922
Dina Sherubayeva <18648182-Anidowia@users.noreply.gitlab.com>
2024-07-26 17:03:48 +0500

    refactor: winner banner styles

--- src/layout/winnerBanner/WinnerBanner.tsx
+++ src/layout/winnerBanner/WinnerBanner.tsx
@@ -13,8 +13,8 @@ const WinnerBanner: React.FC = () => {
 	return (
 		<div className={styles.banner}>
 			<h1>WINNER</h1>
-			{winnerName && <p>First Car: {winnerName}</p>}
-			{winnerTime !== null && <p>TIME: {(winnerTime / 1000).toFixed(2)} s</p>}
+			{winnerName && <h1> {winnerName}</h1>}
+			{winnerTime !== null && <h1>TIME: {(winnerTime / 1000).toFixed(2)} S</h1>}
 		</div>
 	);
 };
